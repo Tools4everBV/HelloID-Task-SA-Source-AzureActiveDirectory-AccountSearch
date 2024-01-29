33c1bafa6957a35f63c40e5dc7a5a8189ef872c1
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2024-01-29 16:48:39 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HelloID-Task-SA-Source-AzureActiveDirectory-AccountSearch
-Azure Active Directory - User account search
+
+## Prerequisites
+- [ ] This script uses the Microsoft Graph API and requires an App Registration with App permissions:
+  - [ ] Read all user’s full profiles by using <b><i>User.Read.All</i></b>
+
+## Description
+
+This code snippet executes the following tasks:
+
+1. Define a wildcard search query `$searchQuery` based on the search parameter `$datasource.searchUser`
+2. Creates a token to connect to the Graph API.
+3. List all users in Azure AD using the API call: [List users](https://learn.microsoft.com/en-us/graph/api/user-list?view=graph-rest-1.0&tabs=http)
+4. Filter down to only users with `$searchQuery` in their `displayName` or `userPrincipalName`
+5. Return a hash table for each user account using the `Write-Output` cmdlet.
+
+> To view an example of the data source output, please refer to the JSON code pasted below.
+
+```json
+{
+    "searchUser": "James"
+}
+```
---
+++ action.ps1
@@ -0,0 +1,65 @@
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+try {
+    $searchValue = $datasource.searchUser
+    $searchQuery = "*$searchValue*"
+          
+    Write-Verbose "Generating Microsoft Graph API Access Token.."
+    $baseUri = "https://login.microsoftonline.com/"
+    $authUri = $baseUri + "$AADTenantID/oauth2/token"
+    $body = @{
+        grant_type    = "client_credentials"
+        client_id     = "$AADAppId"
+        client_secret = "$AADAppSecret"
+        resource      = "https://graph.microsoft.com"
+    }
+
+    $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType 'application/x-www-form-urlencoded'
+    $accessToken = $Response.access_token;
+    Write-Information "Searching for: $searchQuery"
+    
+    #Add the authorization header to the request
+    $authorization = @{
+        Authorization  = "Bearer $accesstoken";
+        'Content-Type' = "application/json";
+        Accept         = "application/json";
+    }
+
+    $baseSearchUri = "https://graph.microsoft.com/"
+    $searchUri = $baseSearchUri + "v1.0/users" + '?$select=Id,UserPrincipalName,displayName,department,jobTitle,companyName' + '&$top=999'
+
+    $azureADUsersResponse = Invoke-RestMethod -Uri $searchUri -Method Get -Headers $authorization -Verbose:$false
+    $azureADUsers = $azureADUsersResponse.value
+    while (![string]::IsNullOrEmpty($azureADUsersResponse.'@odata.nextLink')) {
+        $azureADUsersResponse = Invoke-RestMethod -Uri $azureADUsersResponse.'@odata.nextLink' -Method Get -Headers $authorization -Verbose:$false
+        $azureADUsers += $azureADUsersResponse.value
+    }  
+
+    $users = foreach ($azureADUser in $azureADUsers) {
+        if ($azureADUser.displayName -like $searchQuery -or $azureADUser.userPrincipalName -like $searchQuery) {
+            $azureADUser
+        }
+    }
+    $users = $users | Sort-Object -Property DisplayName
+    $resultCount = @($users).Count
+    Write-Information "Result count: $resultCount"
+        
+    if ($resultCount -gt 0) {
+        foreach ($user in $users) {
+            $returnObject = @{
+                Id                = $user.Id;
+                UserPrincipalName = $user.UserPrincipalName;
+                DisplayName       = $user.DisplayName;
+                Department        = $user.Department;
+                Title             = $user.JobTitle;
+                Company           = $user.CompanyName
+            }
+            Write-Output $returnObject
+        }
+    }
+}
+catch {
+    $errorDetailsMessage = ($_.ErrorDetails.Message | ConvertFrom-Json).error.message
+    Write-Error ("Error searching for AzureAD users. Error: $_" + $errorDetailsMessage)
+}
